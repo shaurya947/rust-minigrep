3075d0c672e7a20bd5393b84170d7e6cc9433903
shaurya947 <shaurya.arora1@gmail.com>
2023-07-09 13:57:02 -0400

    Add unit tests for library crate

--- src/lib.rs
+++ src/lib.rs
@@ -4,10 +4,60 @@ pub fn search_file_path_for_string(
     file_path: &str,
     search_string: &str,
 ) -> Result<Vec<String>, io::Error> {
-    let matches = fs::read_to_string(file_path)?
+    let file_as_string = fs::read_to_string(file_path)?;
+    Ok(
+        search_string_lines_for_string(&file_as_string, search_string)
+            .iter()
+            .map(|&s| String::from(s))
+            .collect(),
+    )
+}
+
+fn search_string_lines_for_string<'a>(source_sting: &'a str, search_string: &str) -> Vec<&'a str> {
+    source_sting
         .lines()
         .filter(|l| l.contains(search_string))
-        .map(|s| s.to_string())
-        .collect();
-    Ok(matches)
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn search_string_lines_0_matches() {
+        assert_eq!(
+            search_string_lines_for_string(get_source_string(), "limousine"),
+            Vec::<&str>::new()
+        );
+    }
+
+    #[test]
+    fn search_string_lines_1_match() {
+        assert_eq!(
+            search_string_lines_for_string(get_source_string(), "every"),
+            vec!["everything"]
+        );
+    }
+
+    #[test]
+    fn search_string_lines_multiple_matches() {
+        assert_eq!(
+            search_string_lines_for_string(get_source_string(), "to"),
+            vec![
+                "you'd like to me to go over",
+                "they say that time's supposed to heal you"
+            ]
+        );
+    }
+
+    fn get_source_string() -> &'static str {
+        "hello, it's me
+I was wondering if after all this time
+you'd like to me to go over
+everything
+they say that time's supposed to heal you
+but I ain't done much healing
+        "
+    }
 }
